e326772b6f8e4217c37862705e59471cdd4845a8
leonardo.ocruz <leonardo.ocruz@PIR06305W10-1>
2022-05-25 14:36:43 -0300

    numero_mes

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace numero_do_mes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_result = new System.Windows.Forms.Label();
+            this.cb_mes = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(408, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Elaborar um sistema onde o usuário digita o número do mês \r\ne exibir o nome do" +
+    " mês correspondente.";
+            // 
+            // lb_result
+            // 
+            this.lb_result.AutoSize = true;
+            this.lb_result.Location = new System.Drawing.Point(66, 139);
+            this.lb_result.Name = "lb_result";
+            this.lb_result.Size = new System.Drawing.Size(27, 13);
+            this.lb_result.TabIndex = 4;
+            this.lb_result.Text = "Mês";
+            // 
+            // cb_mes
+            // 
+            this.cb_mes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_mes.FormattingEnabled = true;
+            this.cb_mes.Location = new System.Drawing.Point(15, 74);
+            this.cb_mes.Name = "cb_mes";
+            this.cb_mes.Size = new System.Drawing.Size(121, 21);
+            this.cb_mes.TabIndex = 5;
+            this.cb_mes.SelectedIndexChanged += new System.EventHandler(this.cb_mes_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 342);
+            this.Controls.Add(this.cb_mes);
+            this.Controls.Add(this.lb_result);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_result;
+        private System.Windows.Forms.ComboBox cb_mes;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numero_do_mes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            //CARREGAR COMBO BOX MANUALMENTE
+
+            cb_mes.Items.Add(01);
+            cb_mes.Items.Add(02);
+            cb_mes.Items.Add(03);
+            cb_mes.Items.Add(04);
+            cb_mes.Items.Add(05);
+            cb_mes.Items.Add(06);
+            cb_mes.Items.Add(07);
+            cb_mes.Items.Add(08);
+            cb_mes.Items.Add(09);
+            cb_mes.Items.Add(10);
+            cb_mes.Items.Add(11);
+            cb_mes.Items.Add(12);
+
+
+
+        }
+
+        private void cb_mes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            int mes = Convert.ToInt32(cb_mes.SelectedItem);
+
+            if (mes == 01)
+            {
+                lb_result.Text = "Janeiro";
+
+            }
+            else if (mes == 02)
+            {
+
+                lb_result.Text = "Fevereiro";
+            }
+            else if (mes == 03)
+            {
+
+                lb_result.Text = "Março";
+            }
+            else if (mes == 04)
+            {
+                lb_result.Text = "Abril";
+
+            }
+            else if (mes == 05)
+            {
+                lb_result.Text = "Maio";
+
+
+            }
+            else if (mes == 06)
+            {
+
+
+                lb_result.Text = "Junho";
+
+            }
+            else if (mes == 07)
+            {
+
+                lb_result.Text = "Julho";
+
+            }
+            else if (mes == 08)
+            {
+                lb_result.Text = "Agosto";
+
+            }
+            else if (mes == 09)
+            {
+
+                lb_result.Text = "Setembro";
+
+
+            }
+            else if (mes == 10)
+            {
+                lb_result.Text = "Outubro";
+
+            }
+            else if(mes == 11)
+            {
+
+                lb_result.Text = "Novembro";
+
+            }
+            else if (mes == 12)
+            {
+
+                lb_result.Text = "Dezembro";
+
+
+            }
+                   
+        }
+    }
+}
